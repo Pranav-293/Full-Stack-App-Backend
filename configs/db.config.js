13c5096f6882eca7cb57245bc1e737746bc4ccdc
Pranav Gupta <pranav.19142@davietjal.org>
2023-03-31 13:00:51 +0530

    first commit

---
+++ configs/db.config.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const URL = "mongodb+srv://Pranav:Pranav1234@cluster0.lrt9fqm.mongodb.net/?retryWrites=true&w=majority";
+
+
+ mongoose.connect(URL).catch((err) => {
+    console.log(err.message);
+  });
+  const db = mongoose.connection;
+  db.on("open", () => {
+    console.log("Database connected");
+  });
